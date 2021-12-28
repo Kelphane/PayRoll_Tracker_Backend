d35b095dfc7b35a2861f979463988b70b9f10abe
Dom B <domlamont@yahoo.com>
2021-12-28 09:50:16 -0800

    Created a ValidateUser Function in User.js in Models Folder

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Joi = required("joi");
 const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
@@ -7,9 +8,28 @@ const userSchema = new Schema({
     password: {type: String, required: true}
 });
 
+const User = mongoose.model('User', userSchema);
+
 async function hashPassword(password){
     const salt = bcrypt.genSalt(10);
     const hash = await bcrypt.hash({password, salt});
     return hash;
+}
+
+function validateUser(User){
+    const schema = Joi.object({
+        username: Joi.string().
+            min(5).
+            max(20).
+            required(),
+        password: Joi.string().min(8).max(15).required()
+    });
+    return schema.validate(User);
+}
+
+module.exports = {
+    User: User,
+    hashPassword: hashPassword,
+    validateUser: validateUser
 }
 
